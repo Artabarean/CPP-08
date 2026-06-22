138789c195e9639ffe03e485bc08e07f47831073
Artabarean <atabarea@c1r14s5.42madrid.com>
2026-06-22 12:27:17 +0200

    ex00

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/22 11:14:04 by atabarea          #+#    #+#             */
+/*   Updated: 2026/06/22 12:19:11 by atabarea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <exception>
+#include <algorithm>
+
+template <typename T>
+void easyfind(T& element, int num)
+{
+	typename T::iterator found = std::find(element.begin(), element.end(), num);
+	if(found != element.end())
+	{
+		std::cout << num << " Found on container" << std::endl;
+	}
+	else
+	{
+		throw	std::runtime_error("Occurrence not found on container");
+	}
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/22 11:14:09 by atabarea          #+#    #+#             */
+/*   Updated: 2026/06/22 12:26:54 by atabarea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+#include <vector>
+
+int	main(void)
+{
+	std::vector<int> container;
+	container.push_back(10);
+	container.push_back(32);
+	container.push_back(1);
+	container.push_back(0);
+	try
+	{
+		easyfind(container, 50);
+		std::cout << "success" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	return (0);
+}
