fab228b0e90d89f750604b8682e3026136b4a059
Artabarean <atabarea@c1r1s6.42madrid.com>
2026-06-23 15:53:59 +0200

    ex01 templates

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -6,7 +6,42 @@
 /*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/22 12:31:56 by atabarea          #+#    #+#             */
-/*   Updated: 2026/06/22 12:31:56 by atabarea         ###   ########.fr       */
+/*   Updated: 2026/06/23 15:35:11 by atabarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Span.hpp"
+template <typename T>
+Span<T>::Span(unsigned int N)
+	: _maxN(N)
+{
+	_integers = new T[N]();
+}
+
+template <typename T>
+Span<T>::~Span(void)
+{
+}
+template <typename T>
+void Span<T>::addNumber(void)
+{
+	
+}
+
+template <typename T>
+int Span<T>::longestSpan(void)
+{
+
+}
+
+template <typename T>
+int Span<T>::shortestSpan(void)
+{
+
+}
+
+template <typename T>
+Span& Span<T>::operator=(const Span& other)
+{
+
+}
--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -6,7 +6,21 @@
 /*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/22 12:31:48 by atabarea          #+#    #+#             */
-/*   Updated: 2026/06/22 12:31:48 by atabarea         ###   ########.fr       */
+/*   Updated: 2026/06/23 15:33:17 by atabarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+template <typename T>
+class Span
+{
+	public:
+		Span(unsigned int N);
+		~Span(void);
+		void addNumber(void);
+		int shortestSpan(void);
+		int longestSpan(void);
+		Span &operator=(const Span& other);
+	private:
+		T *_integers;
+		unsigned int _maxN;
+};
